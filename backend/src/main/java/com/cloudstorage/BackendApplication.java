18d16bc0c4cbbc515de26cd1b2192a25aa84dd60
xyzbruet <mahikurza@gmail.com>
2026-01-22 07:52:29 +0530

    Fix BackendApllication  for production

--- backend/src/main/java/com/cloudstorage/BackendApplication.java
+++ backend/src/main/java/com/cloudstorage/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.cloudstorage;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-
-        // Load .env file
-        Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMissing()
-                .load();
-
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
-
         SpringApplication.run(BackendApplication.class, args);
     }
 }
